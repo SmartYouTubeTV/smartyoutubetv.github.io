8a153cc434c6f0bd6e3b8b15d980ae3f38445864
Yuriy Liskov <firsthash@gmail.com>
2018-12-27 22:19:07 +0200

    autogen: update site

--- scripts/snowstormv144_20131208/snowstorm.js
+++ scripts/snowstormv144_20131208/snowstorm.js
@@ -37,7 +37,7 @@ var snowStorm = (function(window, document) {
 
   // --- less-used bits ---
 
-  this.freezeOnBlur = true;       // Only snow when the window is in focus (foreground.) Saves CPU.
+  this.freezeOnBlur = false;       // Only snow when the window is in focus (foreground.) Saves CPU.
   this.flakeLeftOffset = 0;       // Left margin/gutter space on edge of container (eg. browser window.) Bump up these values if seeing horizontal scrollbars.
   this.flakeRightOffset = 0;      // Right margin/gutter space on edge of container
   this.flakeWidth = 8;            // Max pixel width reserved for snow element
